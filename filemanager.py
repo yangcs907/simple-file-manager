3505ae12928cc5ced5960166fbb90782465666ba
yangcs907 <>
2019-04-02 18:32:45 -0400

    updated features

--- filemanager.py
+++ filemanager.py
@@ -22,7 +22,7 @@ def startProgram():
 def fileManager():
     print("Here is a list of the files")
     print(" ")
-    files = os.listdir("File path goes here") #rename filepath to your current machine
+    files = os.listdir("Place file path here") #rename filepath to your current machine
     for file in files:
         if not "filemanager.py" in file:
             print(file)
@@ -32,29 +32,32 @@ def fileManager():
         startProgram()
     else:
         if option in files:
-            print(" ")
-            print("Press 1 to view contents of " + option)
-            print("Press 2 to overwrite contents of " + option)
-            print("Press 3 to append to " + option)
-            print("Press 4 to empty contents of " + option)
-            print("Press B to go back to filemanager")
-            print("Press H to go back to home page")
-            option_manage = input("> ").lower()
-            if option_manage == "h":
-                startProgram()
-            elif option_manage == "b":
-                fileManager()
-            elif option_manage == "1":
-                read_file(option)
-            elif option_manage == "2":
-                overwite_file(option)
-            elif option_manage == "3":
-                append_file(option)
-            elif option_manage == "4":
-                empty_file(option)
+            manage_chosen_file(option)
         else:
             print("Filename does not exist")
             fileManager()
+
+def manage_chosen_file(file):
+    print("\nPress 1 to view contents of " + file)
+    print("Press 2 to overwrite contents of " + file)
+    print("Press 3 to append to " + file)
+    print("Press 4 to empty contents of " + file)
+    print("Press B to go back to filemanager")
+    print("Press H to go back to home page")
+    option_manage = input("> ").lower()
+    if option_manage == "h":
+        startProgram()
+    elif option_manage == "b":
+        fileManager()
+    elif option_manage == "1":
+        read_file(file)
+    elif option_manage == "2":
+        overwite_file(file)
+    elif option_manage == "3":
+        append_file(file)
+    elif option_manage == "4":
+        empty_file(file)
+
 
 def read_file(inputfile):
     with open(inputfile) as file:
@@ -62,31 +65,31 @@ def read_file(inputfile):
         file_read = file.read()
         print(file_read)
         file.close()
-    file_options_goback()
+    file_options_goback(inputfile)
 
 def overwite_file(inputfile):
     print("\nWhat would you like to overwrite to " + inputfile + "?")
     print("Input text or press B to go back")
     overwrite = input("> ")
     if overwrite == "b" or overwrite == "B":
-        fileManager()
+        manage_chosen_file(inputfile)
     with open(inputfile, "w") as file:
         file.write(overwrite)
         print("\nSuccessfully written to " + inputfile)
         file.close()
-    file_options_goback()
+    file_options_goback(inputfile)
 
 def append_file(inputfile):
     print("\nWhat would you like to append to " + inputfile + "?")
     print("Input text or press B to go back")
     append = input("> ")
     if append == "b" or append == "B":
-        fileManager()
+        manage_chosen_file(inputfile)
     with open(inputfile, "a") as file:
         file.write("\n" + append)
         print("\nSuccessfully appended to " + inputfile)
         file.close()
-    file_options_goback()
+    file_options_goback(inputfile)
 
 def empty_file(inputfile):
     print("\nAre you sure you want to empty contents of " + inputfile + "?")
@@ -97,25 +100,25 @@ def empty_file(inputfile):
             file.truncate()
             print("\nSuccessully emptied " + inputfile)
             file.close()
-        file_options_goback()
+        file_options_goback(inputfile)
     elif empty == "n":
-        fileManager()
+        manage_chosen_file(inputfile)
 
-def file_options_goback():
+def file_options_goback(inputfile):
     print("\n What would you like to do now?")
-    print("Press B to go back to file manager")
+    print("Press B to do something else with " + inputfile)
     print("Press H to go back home")
     option = input("> ")
     if option == "b":
-        fileManager()
+        manage_chosen_file(inputfile)
     elif option == "h":
         startProgram()
     else:
         print("Invalid input")
-        file_options_goback()
+        file_options_goback(inputfile)
 
 def create_file():
-    files = os.listdir("File path goes here") #rename filepath to your current machine
+    files = os.listdir("Place file path here")
     print("\nInput name of file you want to create")
     print("Or press B to go back")
     file_create = input("> ")
@@ -131,7 +134,7 @@ def create_file():
 
 def remove_file():
     print("\nWhich file would you like to remove?")
-    files = os.listdir("File path goes here") #rename filepath to your current machine
+    files = os.listdir("Place file path here") #rename filepath to your current machine
     for file in files:
         if not "filemanager.py" in file:
             print(file)
@@ -156,6 +159,10 @@ def remove_file():
         else:
             print("Filename " + option + "does not exist")
             startProgram()
+
+def exit_program():
+    print("Thank you and have a nice day!")
+    sys.exit
 
 
 startProgram()
